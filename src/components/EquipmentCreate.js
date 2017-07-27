d13d9ada8a499a8f7f845a8b5aa1f78f7b16b356
vanbumi <jsp.dio@gmail.com>
2017-07-27 13:23:20 +0700

    add scrollview at equipmentcreate.js

--- src/components/EquipmentCreate.js
+++ src/components/EquipmentCreate.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import { View, Text } from 'react-native';
-import { Picker, Text } from 'react-native'; 
+import { Picker, Text, ScrollView } from 'react-native'; 
 import { connect } from 'react-redux';
 import { equipmentUpdate, equipmentCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -16,90 +16,92 @@ class EquipmentCreate extends Component {
 
   render() {
     return (
-      <Card>
-        <CardSection>
-          <Input 
-            label="Name"
-            placeholder="equipment name"
-            value={this.props.name}
-            onChangeText={text => this.props.equipmentUpdate({ prop: 'name', value: text })}
-          />
-        </CardSection>
+      <ScrollView>
+        <Card>
+          <CardSection>
+            <Input 
+              label="Name"
+              placeholder="equipment name"
+              value={this.props.name}
+              onChangeText={text => this.props.equipmentUpdate({ prop: 'name', value: text })}
+            />
+          </CardSection>
 
-        <CardSection>
-          <Input 
-            label="Owner"
-            placeholder="owner name"
-            value={this.props.owner}
-            onChangeText={text => this.props.equipmentUpdate({ prop: 'owner', value: text })}
-          />
-        </CardSection>
+          <CardSection>
+            <Input 
+              label="Owner"
+              placeholder="owner name"
+              value={this.props.owner}
+              onChangeText={text => this.props.equipmentUpdate({ prop: 'owner', value: text })}
+            />
+          </CardSection>
 
-        <CardSection>
-          <Input 
-            label="Location"
-            placeholder="lokasi equipment"
-            value={this.props.location}
-            onChangeText={text => this.props.equipmentUpdate({ prop: 'location', value: text })}
-          />
-        </CardSection>
+          <CardSection>
+            <Input 
+              label="Location"
+              placeholder="lokasi equipment"
+              value={this.props.location}
+              onChangeText={text => this.props.equipmentUpdate({ prop: 'location', value: text })}
+            />
+          </CardSection>
 
-        <CardSection>
-          <Input 
-            label="Unit"
-            placeholder="tersedia berapa unit"
-            value={this.props.unit}
-            onChangeText={text => this.props.equipmentUpdate({ prop: 'unit', value: text })}
-          />
-        </CardSection>
+          <CardSection>
+            <Input 
+              label="Unit"
+              placeholder="tersedia berapa unit"
+              value={this.props.unit}
+              onChangeText={text => this.props.equipmentUpdate({ prop: 'unit', value: text })}
+            />
+          </CardSection>
 
-        <CardSection>
-          <Input 
-            label="Price"
-            placeholder="harga sewa"
-            value={this.props.price}
-            onChangeText={text => this.props.equipmentUpdate({ prop: 'price', value: text })}
-          />
-        </CardSection>
+          <CardSection>
+            <Input 
+              label="Price"
+              placeholder="harga sewa"
+              value={this.props.price}
+              onChangeText={text => this.props.equipmentUpdate({ prop: 'price', value: text })}
+            />
+          </CardSection>
 
-        <CardSection sytle={{ flexDirection: 'column' }}>
-          <Text style={styles.pickerTextStyle}>Time</Text>
-          <Picker
-            style={{flex: 1}}
-            selectedValue={this.props.time}
-            onValueChange={value => this.props.equipmentUpdate({ prop: 'time', value })}
-          >
-            <Picker.Item label="Pilih per waktu" value="Pilih per waktu" />
-            <Picker.Item label="Day" value="Day" />
-            <Picker.Item label="Week" value="Week" />
-            <Picker.Item label="Month" value="Month" />
-            <Picker.Item label="Year" value="Year" />
-          </Picker>
-        </CardSection>
+          <CardSection sytle={{ flexDirection: 'column' }}>
+            <Text style={styles.pickerTextStyle}>Time</Text>
+            <Picker
+              style={{flex: 1}}
+              selectedValue={this.props.time}
+              onValueChange={value => this.props.equipmentUpdate({ prop: 'time', value })}
+            >
+              <Picker.Item label="Pilih per waktu" value="Pilih per waktu" />
+              <Picker.Item label="Day" value="Day" />
+              <Picker.Item label="Week" value="Week" />
+              <Picker.Item label="Month" value="Month" />
+              <Picker.Item label="Year" value="Year" />
+            </Picker>
+          </CardSection>
 
-        <CardSection>
-          <Input 
-            label="Description"
-            placeholder="kondisi equipment"
-            value={this.props.description}
-            onChangeText={text => this.props.equipmentUpdate({ prop: 'description', value: text }) }
-            //onChangeText={value => this.props.equipmentUpdate({ prop:description, value }) } bisa ganti value
-          />
-        </CardSection>
+          <CardSection>
+            <Input 
+              label="Description"
+              placeholder="kondisi equipment"
+              value={this.props.description}
+              onChangeText={text => this.props.equipmentUpdate({ prop: 'description', value: text }) }
+              //onChangeText={value => this.props.equipmentUpdate({ prop:description, value }) } bisa ganti value
+            />
+          </CardSection>
 
-        <CardSection>
-          <Input 
-            label="Image"
-            placeholder="gambar equipment"
-          />
-        </CardSection>
+          <CardSection>
+            <Input 
+              label="Image"
+              placeholder="gambar equipment"
+            />
+          </CardSection>
 
-        <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Create
-          </Button>
-        </CardSection>
-      </Card>
+          <CardSection>
+            <Button onPress={this.onButtonPress.bind(this)}>
+              Create
+            </Button>
+          </CardSection>
+        </Card>
+      </ScrollView>
     );
   }
 }
